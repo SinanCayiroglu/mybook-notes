c5e712f4ce35912cc751f5e24e11bf8ec6ade2d6
SinanCayiroglu <cayiroglusinan@live.com>
2024-05-06 09:57:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const db = new pg.Client({
   user: "blogwebapp_user",
   host: "dpg-cnqqsda1hbls73dserag-a.frankfurt-postgres.render.com",
   database: "blogwebapp",
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  password: Fe2acZyC5sfPhgoPDKObWGUcTyYHPnEl,
+  port: 5432,
   ssl: {
     rejectUnauthorized: false // This is required for render.com's SSL configuration
   }
